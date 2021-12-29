7df0340e261c28e5d2a8a29dc9ac43c914b45b35
mfragger <58777037+mfragger@users.noreply.github.com>
2021-12-29 10:02:36 +0800

    added job support

---
+++ Runtime/Jobs/Job.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjectArchitecture.Jobs
+{
+    public abstract class Job : ScriptableObject
+    {
+        [HideInInspector]
+        public int refcount = 0;
+
+        protected int count = 0;
+    }
+}
