dc0574eb360b713baab6a0c15ee92fbd359c81b3
mfragger <58777037+mfragger@users.noreply.github.com>
2022-01-03 00:56:44 +0800

    preprocessed C#9 new

--- Runtime/Tag/Tag.cs
+++ Runtime/Tag/Tag.cs
@@ -5,6 +5,10 @@ namespace ScriptableObjectArchitecture
 {
     public abstract class Tag<T> : ScriptableObject
     {
+#if UNITY_2021_2_OR_NEWER
         public HashSet<T> TaggedObjects = new();
+#else
+        public HashSet<T> TaggedObjects = new HashSet<T>();
+#endif
     }
 }
