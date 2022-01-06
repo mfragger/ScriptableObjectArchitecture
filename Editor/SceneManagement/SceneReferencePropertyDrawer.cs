fca9b309f5cc7ab0cebfa721b6956922828a9d15
mfragger <58777037+mfragger@users.noreply.github.com>
2022-01-06 08:36:42 +0800

    remvoed foldout in sceneReference

--- Editor/SceneManagement/SceneReferencePropertyDrawer.cs
+++ Editor/SceneManagement/SceneReferencePropertyDrawer.cs
@@ -61,57 +61,39 @@ namespace ScriptableObjectArchitecture.Editor.SceneManagement
             // Move this up
             EditorGUI.BeginProperty(position, GUIContent.none, property);
             {
-                // Here we add the foldout using a single line height, the label and change
-                // the value of property.isExpanded
-                property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, lineHeight), property.isExpanded, label);
+                position.height -= lineHeight;
 
-                // Now you want to draw the content only if you unfold this property
-                if (property.isExpanded)
+                var sceneAssetProperty = GetSceneAssetProperty(property);
+
+                // Draw the Box Background
+                position.height -= FOOTER_HEIGHT;
+                GUI.Box(EditorGUI.IndentedRect(position), GUIContent.none, EditorStyles.helpBox);
+                position = boxPadding.Remove(position);
+                position.height = lineHeight;
+
+                // Draw the main Object field
+                label.tooltip = "The actual Scene Asset reference.\nOn serialize this is also stored as the asset's path.";
+
+
+                var sceneControlID = GUIUtility.GetControlID(FocusType.Passive);
+                EditorGUI.BeginChangeCheck();
                 {
-                    // Optional: Indent the content
-                    //EditorGUI.indentLevel++;
-                    //{
+                    // removed the label here since we already have it in the foldout before
+                    sceneAssetProperty.objectReferenceValue = EditorGUI.ObjectField(position, sceneAssetProperty.objectReferenceValue, typeof(SceneAsset), false);
+                }
+                var buildScene = BuildUtils.GetBuildScene(sceneAssetProperty.objectReferenceValue);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    // If no valid scene asset was selected, reset the stored path accordingly
+                    if (buildScene.scene == null) GetScenePathProperty(property).stringValue = string.Empty;
+                }
 
-                    // reduce the height by one line and move the content one line below
-                    position.height -= lineHeight;
-                    position.y += lineHeight;
+                position.y += paddedLine;
 
-                    var sceneAssetProperty = GetSceneAssetProperty(property);
-
-                    // Draw the Box Background
-                    position.height -= FOOTER_HEIGHT;
-                    GUI.Box(EditorGUI.IndentedRect(position), GUIContent.none, EditorStyles.helpBox);
-                    position = boxPadding.Remove(position);
-                    position.height = lineHeight;
-
-                    // Draw the main Object field
-                    label.tooltip = "The actual Scene Asset reference.\nOn serialize this is also stored as the asset's path.";
-
-
-                    var sceneControlID = GUIUtility.GetControlID(FocusType.Passive);
-                    EditorGUI.BeginChangeCheck();
-                    {
-                        // removed the label here since we already have it in the foldout before
-                        sceneAssetProperty.objectReferenceValue = EditorGUI.ObjectField(position, sceneAssetProperty.objectReferenceValue, typeof(SceneAsset), false);
-                    }
-                    var buildScene = BuildUtils.GetBuildScene(sceneAssetProperty.objectReferenceValue);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        // If no valid scene asset was selected, reset the stored path accordingly
-                        if (buildScene.scene == null) GetScenePathProperty(property).stringValue = string.Empty;
-                    }
-
-                    position.y += paddedLine;
-
-                    if (!buildScene.assetGUID.Empty())
-                    {
-                        // Draw the Build Settings Info of the selected Scene
-                        DrawSceneInfoGUI(position, buildScene, sceneControlID + 1);
-                    }
-
-                    // Optional: If enabled before reset the indentlevel
-                    //}
-                    //EditorGUI.indentLevel--;
+                if (!buildScene.assetGUID.Empty())
+                {
+                    // Draw the Build Settings Info of the selected Scene
+                    DrawSceneInfoGUI(position, buildScene, sceneControlID + 1);
                 }
             }
             EditorGUI.EndProperty();
@@ -221,7 +203,6 @@ namespace ScriptableObjectArchitecture.Editor.SceneManagement
             {
                 BuildUtils.OpenBuildSettings();
             }
-
         }
 
         private static SerializedProperty GetSceneAssetProperty(SerializedProperty property)
