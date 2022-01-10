12d8b296e4c6a094b740a24508a0b9f82b050f68
mfragger <58777037+mfragger@users.noreply.github.com>
2022-01-10 22:59:47 +0800

    fixed Sirenix namespace

--- Editor/ProjectSettings/ProjectSettings.cs
+++ Editor/ProjectSettings/ProjectSettings.cs
@@ -1,10 +1,3 @@
-using Newtonsoft.Json.Serialization;
-using Sirenix.OdinInspector.Editor.TypeSearch;
-using System;
-using System.CodeDom;
-using System.Collections;
-using System.Collections.Generic;
-using System.Dynamic;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
