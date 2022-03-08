f5d60f5f9ea1abf2959ba8ffd5f3b525435234ea
mfragger <58777037+mfragger@users.noreply.github.com>
2022-03-09 07:56:30 +0800

    image component

---
+++ Runtime/Tags/Components/UI/Image/ImageTag.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScriptableObjectArchitecture.Tags.Components.UI
+{
+    [CreateAssetMenu(menuName = "SO Architecture/Tags/Components/UI/Image Tag", fileName = "New Tag Name")]
+    public class ImageTag : Tag<Image> { }
+}
